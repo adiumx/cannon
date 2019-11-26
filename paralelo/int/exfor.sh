0d83ab4bd95571cf202a2e99e93419f668bd2e07
root <root@localhost.localdomain>
2019-11-26 10:45:47 -0500

    primeros resultados

--- paralelo/int/exfor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 1 100)
-do
-   	mpirun -np 4 ./check_memory>>ftiemposc16.txt
-done
--- serie/memory.h
+++
@@ -1,5 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-int get_memory_usage_kb(long* vmrss_kb, long* vmsize_kb);
